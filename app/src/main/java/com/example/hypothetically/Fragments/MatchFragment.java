e78ca19385ce74b941822d14abdbadd517ad2a08
fatima angulo barba <fatimaa@vandenrobotics.com>
2022-01-03 18:54:51 -0800

    guys i got it!!!!
    okay so i did the match scouting page thingy!!

--- app/src/main/java/com/example/hypothetically/Fragments/MatchFragment.java
+++ app/src/main/java/com/example/hypothetically/Fragments/MatchFragment.java
@@ -4,21 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.hypothetically.MainActivity;
+import com.example.hypothetically.R;
 import com.example.hypothetically.databinding.FragmentDataBinding;
 import com.example.hypothetically.databinding.FragmentMatchBinding;
 
-public class MatchFragment extends Fragment {
+public class MatchFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private Spinner deviceSpinner;
+    private Spinner teamSpinner;
 
     private FragmentMatchBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentMatchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        deviceSpinner = root.findViewById(R.id.spinner);
+        teamSpinner = root.findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> deviceAdapter = ArrayAdapter.createFromResource(root.getContext(),R.array.devices,android.R.layout.simple_spinner_item);
+        deviceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        deviceSpinner.setAdapter(deviceAdapter);
+
+        deviceSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> teamAdapter = ArrayAdapter.createFromResource(root.getContext(),R.array.teams,android.R.layout.simple_spinner_item);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpinner.setAdapter(deviceAdapter);
+
+        teamSpinner.setOnItemSelectedListener(this);
+
         return root;
     }
 
@@ -26,5 +49,16 @@ public class MatchFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String choice = parent.getItemAtPosition(position).toString();
+        Toast.makeText(binding.getRoot().getContext(), choice, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
